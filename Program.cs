7251814ecd229fc6d9e4a9f69a70a157de5f38d0
chesteroff99 <75223750+chesteroff99@users.noreply.github.com>
2020-12-16 05:21:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0, b = 0, c = 0;
+            bool menu = true;
+            while (menu == true)
+            {
+                Console.WriteLine("1.+");
+                Console.WriteLine("2.-");
+                Console.WriteLine("3.*");
+                Console.WriteLine("4./");
+
+                switch (Console.ReadLine())
+                {
+
+                    case "1"://+
+                        ReadParams();
+                        c = a + b;
+                        Console.WriteLine(a + "+" + b + "=" + c);
+                        break;
+
+                    case "2"://-
+                        ReadParams();
+                        c = a - b;
+                        Console.WriteLine(a + "-" + b + "=" + c);
+
+                        break;
+
+                    case "3"://*
+                        ReadParams();
+                        c = a * b;
+                        Console.WriteLine(a + "*" + b + "=" + c);
+
+                        break;
+
+                    case "4"://\
+                        ReadParams();
+                        c = a / b;
+                        Console.WriteLine(a + "/" + b + "=" + c);
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Нет такой операции!");
+                        Console.WriteLine();
+                        break;
+
+                }
+
+                void ReadParams()
+                {
+                    Console.WriteLine("a:");
+                    a = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("b:");
+                    b = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
